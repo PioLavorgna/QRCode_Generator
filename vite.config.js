57eb6cfb2db5ee1b967e9d1eb62a59420edf65cf
Pio Lav <piolavorgna@gmail.com>
2023-12-20 16:39:35 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'qrcode',
-        'html2canvas'
-      ],
-    },
+  optimizeDeps: {
+    exclude: ['qrcode', 'html2canvas'],
   },
 })
