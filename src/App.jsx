8496420ff453369c475fdf38fb45dcbb54f0f570
Pio Lav <piolavorgna@gmail.com>
2023-12-20 17:11:16 +0100

    fix images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import NavBar from './Navbar'
-import QRCodeGenerator from './QRCode'
+import NavBar from './module/Navbar'
+import QRCodeGenerator from './module/QRCode'
 
 function App() {
   return (
     <>
     <div 
-      className="bg-[url('/src/assets/background.jpg')] bg-cover bg-center bg-no-repeat rounded-lg shadow-md flex flex-col items-center justify-center  min-h-screen ">
+      className="bg-[url('/public/background.jpg')] bg-cover bg-center bg-no-repeat rounded-lg shadow-md flex flex-col items-center justify-center  min-h-screen ">
         <NavBar/>
         <div className='w-full mb-2 py-1'>
           <QRCodeGenerator />
--- src/module/Navbar.jsx
+++ src/module/Navbar.jsx
@@ -5,16 +5,16 @@ function NavBar() {
 
     return ( 
         <div className='flex flex-col w-full max-w-md rounded-lg'>
-            <img src='/src/assets/LogoQrCode.png' className='rounded-lg mb-1 max-w-md'/>
+            <img src='LogoQrCode.png' className='rounded-lg mb-1 max-w-md'/>
             <div id='iconProfile' className='flex justify-end bg-lime-700/60 rounded-lg p-1'>
                 <img
                     onClick={()=>openNewTab('https://www.linkedin.com/in/piolavorgna/')}
-                    src='/src/assets/icons8-linkedin-48.png' 
+                    src='icons8-linkedin-48.png' 
                     alt='icon' 
                     className="w-10 ransition-transform duration-300 transform hover:scale-110"/>
                 <img 
                     onClick={()=>openNewTab('https://github.com/PioLavorgna')}
-                    src='/src/assets/icons8-github-50.png' 
+                    src='icons8-github-50.png' 
                     alt='icon' 
                     className="w-10 bg-yellow-500 rounded-full ransition-transform duration-300 transform hover:scale-110"/>
             </div>
